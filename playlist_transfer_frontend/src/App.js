82f24cebe78ce08e7d65dca784df67efebc53459
Jacob <jacob.echeverry13@gmail.com>
2024-06-12 12:48:00 -0400

    Added routes to components

--- playlist_transfer_frontend/src/App.js
+++ playlist_transfer_frontend/src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/HomePage';
+import SourceOne from './components/SourceOne';
+import SourceTwo from './components/SourceTwo';
+import Login from './components/Login';
+import Playlists from './components/Playlists';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/Login" exact component={Login} />
+          <Route path="/HomePage" exact component={Home} />
+          <Route path="/SourceOne" component={SourceOne} />
+          <Route path="/playlists" component={Playlists} />
+          <Route path="/SourceTwo" component={SourceTwo} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
