d57a6325482150f507756a40eb31ef0ab7c31dd8
Jacob <jacob.echeverry13@gmail.com>
2024-06-12 23:28:53 -0400

    Made changes to app.js to allow for successful compile

--- playlist_transfer_frontend/src/App.js
+++ playlist_transfer_frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/HomePage';
 import SourceOne from './components/SourceOne';
 import SourceTwo from './components/SourceTwo';
 import Login from './components/Login';
 import Playlists from './components/Playlists';
+import SampleComponent from './components/SampleComponent';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Switch>
-          <Route path="/Login" exact component={Login} />
-          <Route path="/HomePage" exact component={Home} />
-          <Route path="/SourceOne" component={SourceOne} />
-          <Route path="/playlists" component={Playlists} />
-          <Route path="/SourceTwo" component={SourceTwo} />
-        </Switch>
-      </div>
+      <Routes>
+        <Route path="/Login" exact component={Login} />
+        <Route path="/HomePage" exact component={Home} />
+        <Route path="/SourceOne" component={SourceOne} />
+        <Route path="/Playlists" component={Playlists} />
+        <Route path="/SourceTwo" component={SourceTwo} />
+        <Route path="/SampleComponent" component={SampleComponent} />
+      </Routes>
     </Router>
   );
 }
