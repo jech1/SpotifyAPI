77000088b69673c959f48ad1817d3c50f9d75133
Jacob <jacob.echeverry13@gmail.com>
2024-06-12 23:38:26 -0400

    note changes

--- transfer_backend/transfer/views.py
+++ transfer_backend/transfer/views.py
@@ -7,3 +7,6 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def sample_api(request):
     return JsonResponse({"message": "Hello from Django!"})
+
+# Allows us to see if the Django server is running
+# by visiting http://localhost:8000/api/sample/ in a browser
